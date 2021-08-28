7fdd3f672399609c218a7f2a380e58e5aedd7b52
Jeronimo Wanhoff <jeronimo@jeronimowanhoff.de>
2021-08-28 17:50:38 +0200

    added maven pljava configuration extracted database connection to PLJavaConnector and configured connection url made PetroVisitor use PLJavaConnector Added Function to Launch.java, so that it can be used in Postgres

---
+++ src/main/java/org/sep3tools/PLJavaConnector.java
@@ -0,0 +1,29 @@
+package org.sep3tools;
+
+
+import java.sql.*;
+
+public class PLJavaConnector {
+
+    private static String m_url = "jdbc:default:connection";
+
+	public static String getS3Name(String sep3Code) throws SQLException {
+		Connection conn = DriverManager.getConnection(m_url);
+		String query =  "SELECT sep3_term from bml.bml_schluesselmapping where bml_codelist='RockNameList' and sep3_code= ?";
+
+		PreparedStatement stmt = conn.prepareStatement(query);
+		stmt.setString(1, sep3Code);
+		ResultSet rs = stmt.executeQuery();
+		rs.next();
+		String ret;
+		ret = rs.getString("sep3_term") ;
+
+		stmt.close();
+		conn.close();
+
+		return (ret);
+
+
+  }
+
+}
