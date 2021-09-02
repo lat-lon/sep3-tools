b862b76fdf323198354153de6126da2c9e51f7ea
Lyn Elisa Goltz <goltz@lat-lon.de>
2021-09-02 07:48:36 +0200

    introduced logging; added LaunchTest

--- src/main/java/org/sep3tools/Launch.java
+++ src/main/java/org/sep3tools/Launch.java
@@ -1,38 +1,29 @@
 package org.sep3tools;
 
-import org.sep3tools.gen.*;
-
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.sep3tools.gen.PetroGrammarLexer;
+import org.sep3tools.gen.PetroGrammarParser;
 
 public class Launch {
 
-    public static void main(String[] args)  {
-        ANTLRInputStream input = new ANTLRInputStream("^ms(r2,r3(tw),gs(lw,r2-r3)),^u(t,lw),^gs(r3,bei(113),nf?)");
-        //ANTLRInputStream input = new ANTLRInputStream("X");
-        PetroGrammarLexer lexer = new PetroGrammarLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        PetroGrammarParser parser = new PetroGrammarParser(tokens);
-        ParseTree tree = parser.schichtbeschreibung();
-        //System.out.println(tree.toStringTree(parser));
-
-        PetroVisitor visitor = new PetroVisitor();
-        System.out.println(visitor.visit(tree));
+    public static void main( String[] args ) {
+        String sep3String = "^ms(r2,r3(tw),gs(lw,r2-r3)),^u(t,lw),^gs(r3,bei(113),nf?)";
+        String visit = parseS3( sep3String );
+        System.out.println( visit );
     }
 
-    public static String parseS3 (String s3String)  {
+    public static String parseS3( String s3String ) {
 
-        ANTLRInputStream input = new ANTLRInputStream(s3String);
-        PetroGrammarLexer lexer = new PetroGrammarLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        PetroGrammarParser parser = new PetroGrammarParser(tokens);
+        ANTLRInputStream input = new ANTLRInputStream( s3String );
+        PetroGrammarLexer lexer = new PetroGrammarLexer( input );
+        CommonTokenStream tokens = new CommonTokenStream( lexer );
+        PetroGrammarParser parser = new PetroGrammarParser( tokens );
         ParseTree tree = parser.schichtbeschreibung();
-        //System.out.println(tree.toStringTree(parser));
 
         PetroVisitor visitor = new PetroVisitor();
-        return (visitor.visit(tree));
+        return ( visitor.visit( tree ) );
     }
-
 
 }
