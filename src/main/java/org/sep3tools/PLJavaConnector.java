c7c5beeef2948a74611c5c688b701f924ffe208a
tfr42 <torsten.friebe@gmail.com>
2022-02-07 13:41:13 +0100

    added spring formatter and applied code style (#19)

--- src/main/java/org/sep3tools/PLJavaConnector.java
+++ src/main/java/org/sep3tools/PLJavaConnector.java
@@ -4,38 +4,40 @@ import java.sql.*;
 
 public class PLJavaConnector {
 
-    private static String m_url = "jdbc:default:connection";
-    private static String wb = "woerterbuch.\"Woerterbuch\"";
-    private static String st = "woerterbuch.\"Schluesseltypen\"";
+	private static String m_url = "jdbc:default:connection";
 
-    public static void setWb(String wb) {
-        PLJavaConnector.wb = wb;
-    }
+	private static String wb = "woerterbuch.\"Woerterbuch\"";
 
-    public static void setSt(String st) {
-        PLJavaConnector.st = st;
-    }
+	private static String st = "woerterbuch.\"Schluesseltypen\"";
 
-    public static String getS3Name(String sep3Code) throws SQLException {
+	public static void setWb(String wb) {
+		PLJavaConnector.wb = wb;
+	}
 
-        Connection conn = DriverManager.getConnection(m_url);
-        //String query = "SELECT Klartext from woerterbuch.Woerterbuch where Kuerzel=";
-        String query = "select \"Kuerzel\", \"Klartext\" from "+ wb + " w join " + st + " s " +
-                "on w.\"Typ\" = s.\"Nebentypbez\" " +
-                "where (s.\"Datenfeld\" = 'PETRO' OR s.\"Datenfeld\" = 'diverse') AND \"Kuerzel\"=";
+	public static void setSt(String st) {
+		PLJavaConnector.st = st;
+	}
 
-        PreparedStatement stmt = conn.prepareStatement(query + "'" + sep3Code + "'");
-        ResultSet rs = stmt.executeQuery();
-        boolean validRS = rs.next();
-        String ret = "";
-        if (validRS)
-            ret = rs.getString(2);
+	public static String getS3Name(String sep3Code) throws SQLException {
 
-        stmt.close();
-        conn.close();
+		Connection conn = DriverManager.getConnection(m_url);
+		// String query = "SELECT Klartext from woerterbuch.Woerterbuch where Kuerzel=";
+		String query = "select \"Kuerzel\", \"Klartext\" from " + wb + " w join " + st + " s "
+				+ "on w.\"Typ\" = s.\"Nebentypbez\" "
+				+ "where (s.\"Datenfeld\" = 'PETRO' OR s.\"Datenfeld\" = 'diverse') AND \"Kuerzel\"=";
 
-        return (ret);
+		PreparedStatement stmt = conn.prepareStatement(query + "'" + sep3Code + "'");
+		ResultSet rs = stmt.executeQuery();
+		boolean validRS = rs.next();
+		String ret = "";
+		if (validRS)
+			ret = rs.getString(2);
 
-    }
+		stmt.close();
+		conn.close();
+
+		return (ret);
+
+	}
 
 }
