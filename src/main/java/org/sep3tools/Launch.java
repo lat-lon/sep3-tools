9464e4f5eff0cb21f49cbe5dc849dfd34168fb7f
Jeronimo Wanhoff <jeronimo@jeronimowanhoff.de>
2022-02-28 09:28:11 +0100

    added parameter for CLI interface

--- src/main/java/org/sep3tools/Launch.java
+++ src/main/java/org/sep3tools/Launch.java
@@ -9,8 +9,30 @@ import org.sep3tools.gen.*;
 public class Launch {
 
 	public static void main(String[] args) {
-		String sep3String = (args.length == 0 || args[0].isEmpty())
-				? "^ms(r2,r3(tw),gs(lw,r2-r3)),^u(t,lw),^gs(r3,bei(113),nf?)" : args[0];
+		String sep3String;
+		if (args.length == 6) {
+			JavaConnector.setUrl(args[0]);
+			JavaConnector.setUser(args[1]);
+			JavaConnector.setPass(args[2]);
+			JavaConnector.setWb(args[3]);
+			JavaConnector.setSt(args[4]);
+			sep3String = args[5];
+		}
+		else if (args.length == 0 || args[0].isEmpty()) {
+			sep3String = "^ms(r2,r3(tw),gs(lw,r2-r3)),^u(t,lw),^gs(r3,bei(113),nf?)";
+		}
+		else if (args.length == 1) {
+			sep3String = args[0];
+		}
+		else {
+			System.out.println("Aufruf mir folgenden Parametern:\n"
+					+ "JDBC-URL DB-User DB-Pass Woerterbuch-Tabelle Schlüsseltypen-Tabelle SEP3-String\n\n"
+					+ "Beispiel für Parameter:\n" + "\"jdbc:postgresql://localhost/petroparser\" " + "\"petroDB\" "
+					+ "\"PetroPass\" " + "\"woerterbuch.\\\"\"Woerterbuch\\\"\" "
+					+ "\"woerterbuch.\\\"\"Schluesseltypen\\\"\" "
+					+ "\"G(fg-gg,ms-gs,mats,mata,grs(tw)),fX-mX(mata),mS(fs,grs,fg-mg2,mx(voe))\"");
+			return;
+		}
 		String visit = parseS3(sep3String);
 		System.out.println(visit);
 	}
