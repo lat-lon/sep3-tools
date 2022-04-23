95a10b8873bcdd2e97a0c1d466a157cbdd95db6a
Jeronimo Wanhoff <jeronimo@jeronimowanhoff.de>
2022-04-23 14:12:15 +0200

    added tests to verify parser handles normal attributes, quotes and unknown characters correctly

--- src/test/java/org/sep3tools/ParserTest.java
+++ src/test/java/org/sep3tools/ParserTest.java
@@ -11,8 +11,12 @@ import org.junit.Test;
 import org.sep3tools.gen.PetroGrammarLexer;
 import org.sep3tools.gen.PetroGrammarParser;
 
+import org.hamcrest.CoreMatchers;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * @author <a href="mailto:friebe@lat-lon.de">Torsten Friebe</a>
+ * @author <a href="mailto:kontakt@jeronimowanhoff.de">Jeronimo Wanhoff</a>
  */
 public class ParserTest {
 
@@ -24,6 +28,53 @@ public class ParserTest {
 		PetroGrammarParser parser = new PetroGrammarParser(tokens);
 		ParseTree tree = parser.schichtbeschreibung();
 		assertThat(tree, is(notNullValue()));
+	}
+
+	/**
+	 * Fails if input string does not equal parsed string. To verify quotes in attributes
+	 * are parsed.
+	 */
+	@Test
+	public void verifyThatAttributesWithQuotesAreParsed() {
+		CharStream input = CharStreams.fromString("^kal(Rfl(\"ca\"))");
+		PetroGrammarLexer lexer = new PetroGrammarLexer(input);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		PetroGrammarParser parser = new PetroGrammarParser(tokens);
+		ParseTree tree = parser.schichtbeschreibung();
+
+		assertThat(tree.getText(), is("^kal(Rfl(\"ca\"))"));
+
+	}
+
+	/**
+	 * Fails if input string does not equal parsed string. To verify attributes without
+	 * quotes are parsed
+	 */
+	@Test
+	public void verifyThatAttributesWithoutQuotesAreParsed() {
+		CharStream input = CharStreams.fromString("^kal(Rfl(ca))");
+		PetroGrammarLexer lexer = new PetroGrammarLexer(input);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		PetroGrammarParser parser = new PetroGrammarParser(tokens);
+		ParseTree tree = parser.schichtbeschreibung();
+
+		assertThat(tree.getText(), is("^kal(Rfl(ca))"));
+
+	}
+
+	/**
+	 * Fails, if input string matches parsed string. Tests if %-character (not in grammar)
+	 * in input is parsed
+	 */
+	@Test
+	public void verifyThatCharacterNotInGrammarIsNotParsed() {
+		CharStream input = CharStreams.fromString("^kal(%Rfl(\"ca\"))");
+		PetroGrammarLexer lexer = new PetroGrammarLexer(input);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		PetroGrammarParser parser = new PetroGrammarParser(tokens);
+		ParseTree tree = parser.schichtbeschreibung();
+
+		assertThat(tree.getText(), not("^kal(%Rfl(\"ca\"))"));
 	}
 
 }
